c188e787965995733c468861e7b1c841f5ea3d65
Nippon662 <felipete009@outlook.com>
2023-12-01 20:47:35 -0300

    tentando fazer search owner

--- pages/search/search.js
+++ pages/search/search.js
@@ -52,7 +52,101 @@ function getAll(query) {
             tableData = '<tr><td>Nenhum registro encontrado.</td></tr>';
             $(tableId + ' tbody').html(tableData);
         })
+}
+function getAll(query) {
+
+    // View da tabela.
+    var tableData = '<ul>';
+
+    // Acessa o endpoint da API.
+    $.get(app.apiBaseURL + "/items/search/" + query)
+
+        // Se deu certo.
+        .done((apiData) => {
+
+            // Debug: exibe dados da API no console.
+            console.log("API Data:", apiData);
+
+            // Conta quantos registros retornaram da API.
+            const numItems = Object.keys(apiData).length;
+
+
+            // Itera cada registro que retornou da API.
+            for (const id in apiData) {
+
+                // Obtém o registro atual para 'item'.
+                const item = apiData[id];
+
+                // Monta a 'view' da tabela.
+                tableData += `
+                        <li class="items" data-id="${item.id}">
+                                ${item.name}
+                        </li>                   
+                    `;
+            }
+            tableData += '</ul>'
+
+            $('#results').html(tableData);
+            $('.items').click(getClickedItem);
+
+        })
+
+        // Se falhou ou ler a API...
+        .fail((error) => {
+
+            // Monta e exibe uma mensagem de erro dentro da tabela correspondente.
+            tableData = '<tr><td>Nenhum registro encontrado.</td></tr>';
+            $(tableId + ' tbody').html(tableData);
+        })
+}
+
+function getAll(query) {
+
+    // View da tabela.
+    var tableData = '<ul>';
+
+    // Acessa o endpoint da API.
+    $.get(app.apiBaseURL + "/items/search/" + query)
+
+        // Se deu certo.
+        .done((apiData) => {
+
+            // Debug: exibe dados da API no console.
+            console.log("API Data:", apiData);
+
+            // Conta quantos registros retornaram da API.
+            const numItems = Object.keys(apiData).length;
+
+
+            // Itera cada registro que retornou da API.
+            for (const id in apiData) {
+
+                // Obtém o registro atual para 'item'.
+                const item = apiData[id];
+
+                // Monta a 'view' da tabela.
+                tableData += `
+                            <li class="items" data-id="${item.id}">
+                                    ${item.name}
+                            </li>                   
+                        `;
+            }
+            tableData += '</ul>'
+
+            $('#results').html(tableData);
+            $('.items').click(getClickedItem);
+
+        })
+
+        // Se falhou ou ler a API...
+        .fail((error) => {
+
+            // Monta e exibe uma mensagem de erro dentro da tabela correspondente.
+            tableData = '<tr><td>Nenhum registro encontrado.</td></tr>';
+            $(tableId + ' tbody').html(tableData);
+        })
 
     // Termina sem fazer mais nada.
     return false;
 }
+
